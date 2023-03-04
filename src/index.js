04eef45045db7546759b66ce0e3f97c25ced986d
DeepakBarwal <DeepakBarwal98@gmail.com>
2023-03-04 10:17:00 +0530

    Added jwt create and verify logic

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const app = express();
 const { PORT } = require("./config/serverConfig");
 const apiRoutes = require("./routes/index");
 
+const UserService = require("./services/user-service");
+
 const prepareAndStartServer = () => {
 
     app.use(express.urlencoded({ extended: true }));
@@ -13,6 +15,12 @@ const prepareAndStartServer = () => {
 
     app.listen(PORT, () => {
         console.log(`Server started on PORT: ${PORT}`);
+
+        const userService = new UserService();
+        // const token = userService.createToken({email: 'test@test.com', id: 1});
+        // console.log(token);
+        const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3RAdGVzdC5jb20iLCJpZCI6MSwiaWF0IjoxNjc3OTA1MDcxLCJleHAiOjE2Nzc5MDg2NzF9.XuOUez3ID0rrYSAnEoPgYWqv2zUhPX-W5Y5kBB5xNS4'
+        console.log(userService.verifyToken(token));
     });
 }
 
